e86dc63c58f9fab828313fefdbcefe36add320f7
Potato-TW <85781433+Potato-TW@users.noreply.github.com>
2023-05-17 04:39:53 +0800

    change name and struction

--- Dungeon/Monster.cpp
+++
@@ -1,7 +0,0 @@
-#include "Monster.h"
-
-Monster::Monster(string _name, string _tag, int _maxHealth, int _sp,
-    int _attack, int _defense) :
-    GameCharacter(_name, _tag, _maxHealth, _sp, _attack, _defense) {};
-
-string Monster::triggerEvent(Object* object) { return "1"; }
